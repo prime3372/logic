40afbd701b3ef80982d6b8b98ca9283676f44e3a
prime3372 <syunrurusera@yahoo.ne.jp>
2026-07-11 19:26:48 +0900

    a

---
+++ equiv_and_and_p_q_r_and_p_and_q_r.cpp
@@ -0,0 +1,26 @@
+#include "PropositionalLogic.hpp"
+
+using P = Prop<0>;
+using Q = Prop<1>;
+using R = Prop<2>;
+
+consteval Equiv<And<And<P, Q>, R>, And<P, And<Q, R>>> solve() {
+    return {
+        [&](And<And<P, Q>, R> and_and_p_q_r) -> And<P, And<Q, R>> {
+            return {
+                and_and_p_q_r.left.left,
+                {and_and_p_q_r.left.right, and_and_p_q_r.right}
+            };
+        },
+        [&](And<P, And<Q, R>> and_p_and_q_r) -> And<And<P, Q>, R> {
+            return {
+                {and_p_and_q_r.left, and_p_and_q_r.right.left},
+                and_p_and_q_r.right.right
+            };
+        }
+    };
+}
+
+int main() {
+    solve();
+}
