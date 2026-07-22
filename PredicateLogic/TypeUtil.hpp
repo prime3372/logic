1b818882d7838da53107b96a1910c0902058a329
prime3372 <syunrurusera@yahoo.ne.jp>
2026-07-23 03:09:35 +0900

    a

--- PredicateLogic/TypeUtil.hpp
+++ PredicateLogic/TypeUtil.hpp
@@ -3,54 +3,28 @@
 #include <cstddef>
 #include <tuple>
 
-template <class Tuple, template <class...> class Target>
-class ApplyType_Class {
-    static_assert(false);
-};
-
-template <class... Args, template <class...> class Target>
-class ApplyType_Class<std::tuple<Args...>, Target> {
-public:
-    using type = Target<Args...>;
-};
-
-template <class Tuple, template <class...> class Target>
-using ApplyType = typename ApplyType_Class<Tuple, Target>::type;
-
+template <class T, class U, class V>
+class ReplaceTypeImplementation;
 
 template <class T, class U, class V>
-class ReplaceType_Class;
+using ReplaceType = typename ReplaceTypeImplementation<T, U, V>::result;
 
-template <class T, class U, class V>
-using ReplaceType = typename ReplaceType_Class<T, U, V>::type;
+template <template <class...> class Template, class TemplateArgs, class U, class V>
+class ReplaceTypeHelper {};
 
-template <class Tuple, class U, class V, class Is = std::make_index_sequence<std::tuple_size_v<Tuple>>>
-class ReplaceTypeHelper_Class {
-    static_assert(false);
+template <template <class...> class Template, class U, class V, class... Args>
+class ReplaceTypeHelper<Template, std::tuple<Args...>, U, V> {
+public:
+    using result = Template<ReplaceType<Args, U, V>...>;
 };
 
-template <class Tuple, class U, class V, size_t... Is>
-class ReplaceTypeHelper_Class<Tuple, U, V, std::index_sequence<Is...>> {
-public:
-    using type =
-        std::tuple<
-            ReplaceType<std::tuple_element_t<Is, Tuple>, U, V>...
-        >;
-};
-
-template <class Tuple, class U, class V>
-using ReplaceTypeHelper = ReplaceTypeHelper_Class<Tuple, U, V>::type;
-
 template <class T, class U, class V>
-class ReplaceType_Class {
+class ReplaceTypeImplementation {
 public:
-    using type =
+    using result =
         std::conditional_t<
             std::same_as<T, U>,
             V,
-            ApplyType<
-                ReplaceTypeHelper<typename T::TemplateArgs, U, V>,
-                T::template Template
-            >
+            typename ReplaceTypeHelper<T::template Template, typename T::TemplateArgs, U, V>::result
         >;
 };
